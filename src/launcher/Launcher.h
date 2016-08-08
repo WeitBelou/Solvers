25ec8aca447be799f322cff137b609d172c0d1df
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-08-08 15:42:03 +0300

    Initial commit

---
+++ src/launcher/Launcher.h
@@ -0,0 +1,15 @@
+//
+// Created by ivan on 08.08.16.
+//
+
+#ifndef ELASTICITY_LAUNCHER_H
+#define ELASTICITY_LAUNCHER_H
+
+
+class Launcher
+{
+
+};
+
+
+#endif //ELASTICITY_LAUNCHER_H
---
+++ src/libelast/Problem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 08.08.16.
+//
+
+#include "Problem.h"
