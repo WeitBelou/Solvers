dc11075a94efa060ca86150a4c11e0353ce139e8
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-08-13 21:29:23 +0300

    Delete

---
+++ src/launcher/util/TaskReader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 11.08.16.
+//
+
+#include "TaskReader.hpp"
---
+++ src/launcher/util/TaskReader.hpp
@@ -0,0 +1,10 @@
+//
+// Created by ivan on 11.08.16.
+//
+
+#ifndef SOLVERS_TASKREADER_HPP
+#define SOLVERS_TASKREADER_HPP
+
+
+
+#endif //SOLVERS_XML_HPP
