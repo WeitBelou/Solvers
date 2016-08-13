c9b4aea17e50776acc56d195ad1a94e70e7e0f16
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-08-13 21:35:23 +0300

    Delete example

--- src/launcher/Launcher.cpp
+++ src/launcher/Launcher.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 
 #include "Launcher.h"
-#include "src/libsolvers/LaplaceSolver/PrimalSolver.hpp"
-#include "src/libsolvers/Postprocessor/OutputResults.hpp"
 
 Launcher::Launcher ()
 {
@@ -40,34 +38,9 @@ void Launcher::run ()
               << " from: " << bfs::canonical (taskName).remove_filename ()
               << std::endl;
 
-    std::cout << "Output will be writed in: " << bfs::canonical (outputDir)
+    std::cout << "Output will be writen in: " << bfs::canonical (outputDir)
               << std::endl;
-
-    dealii::Triangulation<2> triangulation;
-    dealii::GridGenerator::hyper_cube (triangulation, 0.0, 1.0);
-
-    dealii::FE_Q<2> fe (2);
-    dealii::QGauss<2> quadrature (2);
-    dealii::ZeroFunction<2> zero (1);
-    dealii::ConstantFunction<2> one (1.0, 2);
-
-    LaplaceSolver::PrimalSolver<2> solver (triangulation,
-                                           fe,
-                                           quadrature,
-                                           zero,
-                                           one);
-    while (solver.n_dofs () < 10000)
-    {
-        solver.refine_grid ();
-        solver.solve_problem ();
-    }
-
-    Postprocessor::OutputResults<2>
-        outputResults (outputDir.string (),
-                       "solution",
-                       dealii::DataOutBase::vtu);
-
-    solver.postprocess (outputResults);
 
     std::cout << "Task completed." << std::endl;
 }
+
