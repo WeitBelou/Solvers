18d2a2f409d96dc8b165554e921c56dd312000c1
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2016-08-19 20:20:50 +0300

    Add task reader scratch

--- src/launcher/util/TaskReader.hpp
+++ src/launcher/util/TaskReader.hpp
@@ -5,6 +5,13 @@
 #ifndef SOLVERS_TASKREADER_HPP
 #define SOLVERS_TASKREADER_HPP
 
+#include <deal.II/base/parameter_handler.h>
 
+namespace TaskReader
+{
+    using namespace dealii;
+
+
+}
 
 #endif //SOLVERS_XML_HPP
